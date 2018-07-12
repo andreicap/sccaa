69de64f717e8c6e12502b34363d141c874704375
andreicap <andrei.capastru@gmail.com>
2018-07-12 16:38:43 +0200

    fix main

--- main.cpp
+++ main.cpp
@@ -9,30 +9,46 @@ int main(int argc, char const *argv[])
   graph_t g;
   vector<vertex_t> vertices;
 
-  int v_number = 5;
+  int v_number = strtol(argv[1], NULL, 10);
   int e_number = v_number*2;
-  boost::mt19937 rng;
-  rng.seed(uint32_t(time(0)));
-  boost::generate_random_graph(g, v_number, e_number, rng);
-  // size_t e = boost::num_edges(graph);
-  // size_t n = boost::num_vertices(graph);
+  
+  //boost random generator
+  
+  // boost::mt19937 rng;
+  // rng.seed(uint32_t(time(0)));
+  // boost::generate_random_graph(g, v_number, e_number, rng);
   
 
+  //erdos generator
+  float epsilon = 0.17;
+  boost::minstd_rand gen;
+  gen.seed(uint32_t(time(0)));
+  graph_t gr(ERGen(gen, v_number, epsilon), ERGen(), v_number);
+  g = gr;
+
+
+
+
+
   print_graph(g);
+  
   //boost implementation
   readSCC(g);
 
-  // nuutila_recursive(g);
+  //nuutila implementaion
+  nuutila_recursive(g);
   
-  //pearce recursive first implementation
+
+  //pearce improved
   pr2 (g);
 
-  // pr1 (g);
+  //pearce recursive first implementation
+  pr1 (g);
 
+  
 
- 
-  //tarjan
-  // tarjan_recursive(g);
+  //tarjan implementation
+  tarjan_recursive(g);
 
   return 0;
   
